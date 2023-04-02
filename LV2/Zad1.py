d596a64c4292b0a13d4d508e75df730d6d2ab48f
Mateo Spajić <101796404+Spajki001@users.noreply.github.com>
2023-04-03 00:45:28 +0200

    Add files via upload

---
+++ LV2/Zad1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 3, 1]
+y = [1, 2, 2, 1, 1]
+plt.plot(x, y, linewidth=1, marker='*', color='red')
+plt.axis([0, 4, 0, 4])
+plt.xlabel('x os')
+plt.ylabel('y os')
+plt.title('Example')
+plt.show()
